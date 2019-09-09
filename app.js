5a7d782e4925bc0e2049638adeba2d3676d2ecce
Jerkbert <raballantyne@gmail.com>
2019-09-09 16:24:39 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const BASE_URL = "localhost:3000"
+// const BASE_URL = "https://damp-dawn-00549.herokuapp.com/"
+
+const ARTIST_URL = `${BASE_URL}/artists/`
+const PROJECT_URL = `${BASE_URL}/projects/`
+const SONG_URL = `${BASE_URL}/songs/`
+const PART_URL = `${BASE_URL}/parts/`
+
+function parseJson(response){
+    return response.json();
+}
+
+
+
+
+
+
